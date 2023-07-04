1cc7674a89fd2b8f92f974381146340dba6ec2dc
HellCatten <126400932+HellCatten@users.noreply.github.com>
2023-07-04 22:50:55 +0300

    Конец?

--- app/src/main/java/com/example/mireaapp/MainActivity.java
+++ app/src/main/java/com/example/mireaapp/MainActivity.java
@@ -259,6 +259,7 @@ public class MainActivity extends AppCompatActivity {
                         progressDialog.dismiss();
                     }
                 }
+
             });
         });
         t.start();
